eb7c2de0bd4040fc8d7c2c505dbd50fcc494ae8a
providencepublishers247-jpg <providencepublishers247@gmail.com>
2026-05-30 04:09:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,6 +24,5 @@ def home():
         """
 
     return render_template("index.html", questions=questions)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
